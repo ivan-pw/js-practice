963fa7620a6d1313cee4614758f019025e073b79
Ivan Zviagin <ivan@ivan.pw>
2023-03-11 15:52:24 +0400

    add forms

---
+++ Project_1/src/js/modules/forms.js
@@ -0,0 +1,76 @@
+const forms = () => {
+  const form = document.querySelectorAll('form');
+  const inputs = document.querySelectorAll('input');
+  const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+  phoneInputs.forEach((item) => {
+    item.addEventListener('input', () => {
+      item.value = item.value.replace(/\D/, '');
+
+      const x = item.value
+        .replace(/\D/g, '')
+        .match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+      item.value = !x[2]
+        ? `${x[1]}`
+        : `(${x[1]}) ${x[2]}${x[3] ? `-${x[3]}` : ''}`;
+    });
+  });
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро наш менеджер Вам перезвонит',
+    failure: 'Что-то пошло не так :(',
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading;
+
+    const res = await fetch(url, {
+      method: 'POST',
+      body: data,
+    });
+
+    const text = await res.text();
+
+    return text;
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((item) => {
+      item.value = '';
+    });
+  };
+
+  form.forEach((item) => {
+    item.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+
+      postData('assets/server.php', formData)
+        .then((res) => {
+          // console.log(res);
+          if (res.ok) {
+            statusMessage.textContent = message.success;
+          } else {
+            statusMessage.textContent = message.failure;
+          }
+        })
+        .catch(() => {
+          statusMessage.textContent = message.failure;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        });
+    });
+  });
+};
+
+export default forms;
