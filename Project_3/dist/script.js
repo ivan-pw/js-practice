217b05d7795d2c4a60d0809cf4bff4d5a022178f
Ivan Zviagin <ivan@ivan.pw>
2023-04-06 21:07:00 +0400

    add slider prot

--- Project_3/dist/script.js
+++ Project_3/dist/script.js
@@ -1,1 +1,264 @@
-!function(){"use strict";class e{constructor(e,s){this.page=document.querySelector(e),this.slides=this.page.children,this.btns=document.querySelectorAll(s),this.slideIndex=1}showSlides(e){e>this.slides.length&&(this.slideIndex=1),e<1&&(this.slideIndex=this.slides.length);try{this.hanson.style.opacity="0",3===e?(this.hanson.classList.add("animated"),setTimeout((()=>{this.hanson.style.opacity="1",this.hanson.classList.add("slideInUp")}),3e3)):this.hanson.classList.remove("slideInUp")}catch(e){console.log(e)}Array.from(this.slides).forEach((e=>{e.style.display="none"})),this.slides[this.slideIndex-1].style.display="block"}plusSlides(e){this.showSlides(this.slideIndex+=e)}render(){try{this.hanson=document.querySelector(".hanson")}catch(e){console.log(e)}this.btns.forEach((e=>{e.addEventListener("click",(()=>{this.plusSlides(1)})),e.parentNode.previousElementSibling.addEventListener("click",(e=>{e.preventDefault(),this.slideIndex=1,this.showSlides(this.slideIndex)}))})),this.showSlides(this.slideIndex)}}class s{constructor(e,s){this.btns=document.querySelectorAll(e),this.overlay=document.querySelector(s),this.close=this.overlay.querySelector(".close")}createPlayer(e){this.player=new YT.Player("frame",{height:"100%",width:"100%",videoId:e}),console.log(this.player),this.overlay.style.display="flex"}bindTriggers(){this.btns.forEach((e=>{e.addEventListener("click",(()=>{const s=e.dataset.url;console.log(s),this.createPlayer(s)}))}))}bindCloseBtn(){this.close.addEventListener("click",(()=>{this.player.destroy(),this.overlay.style.display="none"}))}init(){const e=document.createElement("script");e.src="https://www.youtube.com/iframe_api";const s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(e,s),this.bindTriggers(),this.bindCloseBtn()}}window.addEventListener("DOMContentLoaded",(()=>{new e(".page",".next").render(),new s(".showup .play",".overlay").init()}))}();
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/playVideo.js":
+/*!*************************************!*\
+  !*** ./src/js/modules/playVideo.js ***!
+  \*************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ VideoPlayer; }
+/* harmony export */ });
+class VideoPlayer {
+  constructor(triggers, overlay) {
+    this.btns = document.querySelectorAll(triggers);
+    this.overlay = document.querySelector(overlay);
+    this.close = this.overlay.querySelector('.close');
+  } // play() {
+  //   this.btns.forEach((btn) => {
+  //     btn.addEventListener('click', () => {
+  //       console.log('hello!');
+  //     });
+  //   });
+  // }
+
+
+  createPlayer(url) {
+    this.player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: url // playerVars: {
+      //   playsinline: 1,
+      // },
+      // events: {
+      //   onReady: onPlayerReady,
+      //   onStateChange: onPlayerStateChange,
+      // },
+
+    });
+    console.log(this.player);
+    this.overlay.style.display = 'flex';
+  }
+
+  bindTriggers() {
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        // if (document.querySelector('iframe#frame')) {
+        //   this.overlay.style.display = 'flex';
+        // } else {
+        //   const path = btn.dataset.url;
+        //   this.createPlayer(path);
+        // }
+        const path = btn.dataset.url;
+        console.log(path);
+        this.createPlayer(path);
+      });
+    });
+  }
+
+  bindCloseBtn() {
+    this.close.addEventListener('click', () => {
+      this.player.destroy(); // this.player.stopVideo();
+
+      this.overlay.style.display = 'none';
+    });
+  }
+
+  init() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.bindTriggers();
+    this.bindCloseBtn();
+  }
+
+}
+
+/***/ }),
+
+/***/ "./src/js/modules/slider/slider-main.js":
+/*!**********************************************!*\
+  !*** ./src/js/modules/slider/slider-main.js ***!
+  \**********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ MainSlider; }
+/* harmony export */ });
+/* harmony import */ var _slider__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./slider */ "./src/js/modules/slider/slider.js");
+
+class MainSlider extends _slider__WEBPACK_IMPORTED_MODULE_0__["default"] {
+  // constructor(page, btns) {
+  //   super(page, btns);
+  // }
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+
+    try {
+      this.hanson.style.opacity = '0';
+
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('slideInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('slideInUp');
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    Array.from(this.slides).forEach(slide => {
+      slide.style.display = 'none';
+    });
+    this.slides[this.slideIndex - 1].style.display = 'block';
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) {
+      console.log(e);
+    }
+
+    this.btns.forEach(item => {
+      item.addEventListener('click', () => {
+        this.plusSlides(1);
+      });
+      item.parentNode.previousElementSibling.addEventListener('click', e => {
+        e.preventDefault();
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
+      });
+    });
+    this.showSlides(this.slideIndex);
+  }
+
+}
+
+/***/ }),
+
+/***/ "./src/js/modules/slider/slider.js":
+/*!*****************************************!*\
+  !*** ./src/js/modules/slider/slider.js ***!
+  \*****************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Slider; }
+/* harmony export */ });
+class Slider {
+  constructor({
+    page = '',
+    btns = '',
+    next = '',
+    prev = ''
+  } = {}) {
+    this.page = document.querySelector(page);
+    this.slides = this.page.children;
+    this.btns = document.querySelectorAll(btns);
+    this.slideIndex = 1;
+  }
+
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_slider_slider_main__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/slider/slider-main */ "./src/js/modules/slider/slider-main.js");
+/* harmony import */ var _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/playVideo */ "./src/js/modules/playVideo.js");
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const mainSlider = new _modules_slider_slider_main__WEBPACK_IMPORTED_MODULE_0__["default"]({
+    page: '.page',
+    btns: '.next'
+  });
+  mainSlider.render();
+  const player = new _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__["default"]('.showup .play', '.overlay');
+  player.init();
+});
+}();
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
