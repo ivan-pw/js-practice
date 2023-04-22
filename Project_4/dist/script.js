9e1b9814708040ef0567df7ff64ae2273eadd321
Ivan Zviagin <ivan@ivan.pw>
2023-04-22 14:03:24 +0400

    add tabs

--- Project_4/dist/script.js
+++ Project_4/dist/script.js
@@ -116,6 +116,27 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.createModal = function (
 
 /***/ }),
 
+/***/ "./src/js/lib/components/tabs.js":
+/*!***************************************!*\
+  !*** ./src/js/lib/components/tabs.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.tab = function () {
+  for (let i = 0; i < this.length; i++) {
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).on('click', e => {
+      e.preventDefault();
+      (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).addClass('tab-item--active').siblings().removeClass('tab-item--active').closest('.tab').find('.tab-content').removeClass('tab-content--active').eq((0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).index()).addClass('tab-content--active');
+    });
+  }
+};
+(0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-panel] .tab-item').tab();
+
+/***/ }),
+
 /***/ "./src/js/lib/core.js":
 /*!****************************!*\
   !*** ./src/js/lib/core.js ***!
@@ -167,6 +188,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_effects__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/effects */ "./src/js/lib/modules/effects.js");
 /* harmony import */ var _components_dropdown__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/dropdown */ "./src/js/lib/components/dropdown.js");
 /* harmony import */ var _components_modal__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/modal */ "./src/js/lib/components/modal.js");
+/* harmony import */ var _components_tabs__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/tabs */ "./src/js/lib/components/tabs.js");
+
 
 
 
@@ -211,15 +234,15 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.eq = function (i) {
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.index = function () {
   const parent = this[0].parentNode;
   const childs = [...parent.children];
-  const findMyIndex = item => {
-    return item == this[0];
-  };
+  const findMyIndex = item => item == this[0];
   return childs.findIndex(findMyIndex);
 };
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selector) {
   let numberOfItems = 0;
   let counter = 0;
-  const copyObj = Object.assign({}, this);
+  const copyObj = {
+    ...this
+  };
   for (let i = 0; i < copyObj.length; i++) {
     const arr = copyObj[i].querySelectorAll(selector);
     if (arr.length == 0) {
@@ -253,7 +276,9 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (sele
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
   let numberOfItems = 0;
   let counter = 0;
-  const copyObj = Object.assign({}, this);
+  const copyObj = {
+    ...this
+  };
   for (let i = 0; i < copyObj.length; i++) {
     const arr = copyObj[i].parentNode.children;
     for (let j = 0; j < arr.length; j++) {
